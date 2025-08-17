85b48619c881251d83873c7547e20187d09eae48
Loutimi <loutimi59@gmail.com>
2025-08-17 13:24:10 +0100

    Modify the payment model to add checkout_url field

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -88,5 +88,6 @@ class Payment(models.Model):
     transaction_id = models.CharField(max_length=100, unique=True)  # tx_ref
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    checkout_url = models.URLField(max_length=500, null=True, blank=True)  
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
