aa9bc79cc159839413ff0acf6272e8404b561cda
Loutimi <loutimi59@gmail.com>
2025-08-17 13:26:02 +0100

    Add payment serializer

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Listing, Booking, Review, Payment
+from django.conf import settings
 
 
 class ListingSerializer(serializers.ModelSerializer):
@@ -31,6 +32,8 @@ class BookingSerializer(serializers.ModelSerializer):
     listing = serializers.PrimaryKeyRelatedField(queryset=Listing.objects.all())
     user = serializers.StringRelatedField(read_only=True)
     average_rating = serializers.FloatField(source="listing.average_rating", read_only=True)
+    checkout_url = serializers.CharField(read_only=True)  # dynamically added after payment
+    tx_ref = serializers.CharField(read_only=True)
 
     class Meta:
         model = Booking
@@ -43,24 +46,26 @@ class BookingSerializer(serializers.ModelSerializer):
             'total_price',
             'status',
             'created_at',
-            'average_rating'
+            'average_rating',
+            'checkout_url',
+            'tx_ref'
         ]
-        read_only_fields = ['booking_id', 'user', 'total_price', 'created_at']
+        read_only_fields = ['booking_id', 'user', 'total_price', 'created_at', 'checkout_url', 'tx_ref']
 
     def validate(self, data):
-        # 1. End date check
+        # End date check
         if data['start_date'] >= data['end_date']:
             raise serializers.ValidationError("End date must be after start date.")
 
-        # 2. Overlapping booking check
+        # Overlapping booking check
         listing = data['listing']
         start_date = data['start_date']
         end_date = data['end_date']
 
         overlapping = Booking.objects.filter(
             listing=listing,
-            start_date__lt=end_date,   # booking starts before this one ends
-            end_date__gt=start_date    # booking ends after this one starts
+            start_date__lt=end_date,
+            end_date__gt=start_date
         )
 
         if self.instance:
@@ -78,15 +83,15 @@ class BookingSerializer(serializers.ModelSerializer):
         nights = (validated_data['end_date'] - validated_data['start_date']).days
         validated_data['total_price'] = listing.price_per_night * nights
 
-        return super().create(validated_data)
-    
+        booking = super().create(validated_data)
+        return booking
+
     def update(self, instance, validated_data):
         instance = super().update(instance, validated_data)
         nights = (instance.end_date - instance.start_date).days
         instance.total_price = instance.listing.price_per_night * nights
         instance.save()
         return instance
-
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -115,4 +120,4 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
-        read_only_fields = ("status", "transaction_id", "created_at", "updated_at")
+        read_only_fields = ("status", "transaction_id", "checkout_url", "created_at", "updated_at")
